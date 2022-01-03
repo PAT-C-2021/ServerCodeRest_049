7585842152e71e998a9d77652bbcc6acb8c64ea8
HafidzEkaPradana <hafidz.eka.2016@ft.umy.ac.id>
2022-01-03 13:17:10 +0700

    Add project files.

---
+++ ServerCodeRest_20160140049_HafidzEkaPradana/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceRest_20160140049_HafidzEkaPradana;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.ServiceModel;
+
+namespace ServerCodeRest_20160140049_HafidzEkaPradana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null; 
+            Uri address = new Uri("http://localhost:1907/Mahasiswa"); 
+            WebHttpBinding binding = new WebHttpBinding();
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY), address); 
+                hostObjek.AddServiceEndpoint(typeof(ITI_UMY), binding, "");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior(); 
+                smb.HttpGetEnabled = true; 
+                hostObjek.Description.Behaviors.Add(smb);
+                Binding mexbinding = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObjek.AddServiceEndpoint(typeof(IMetadataExchange), mexbinding, "mex");
+                
+                WebHttpBehavior whb = new WebHttpBehavior();
+                whb.HelpEnabled = true;
+                hostObjek.Description.Endpoints[0].EndpointBehaviors.Add(whb);
+
+                hostObjek.Open();
+                Console.WriteLine("Server ready ..");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            
+            
+            
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            }
+    }
+}
